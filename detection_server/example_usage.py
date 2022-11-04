88f4d3e1db95243947b7f5601b263339c497898d
BryannYeap <e0543723@u.nus.edu>
2022-11-04 20:08:53 +0800

    Add database API

---
+++ detection_server/example_usage.py
@@ -0,0 +1,10 @@
+import database
+
+# Add data example
+
+# database.add_sensor_data({
+#     "temperature": 29.1,
+#     "sound":  "50V",
+#     "infrared": "300V",
+#     "actuation_triggered": False,
+# })
